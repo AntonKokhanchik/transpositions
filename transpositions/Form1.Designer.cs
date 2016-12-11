d9fc01b5b01aa2251b3d7713ec973c584e5f8717
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2016-12-11 14:27:25 +0300

    think that done

---
+++ transpositions/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace transpositions
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxNumber = new System.Windows.Forms.TextBox();
+			this.textBoxLexOut = new System.Windows.Forms.TextBox();
+			this.textBoxMinOut = new System.Windows.Forms.TextBox();
+			this.buttonRun = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(207, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Количество элементов в перестановке";
+			// 
+			// textBoxNumber
+			// 
+			this.textBoxNumber.Location = new System.Drawing.Point(12, 25);
+			this.textBoxNumber.Name = "textBoxNumber";
+			this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+			this.textBoxNumber.TabIndex = 1;
+			// 
+			// textBoxLexOut
+			// 
+			this.textBoxLexOut.Location = new System.Drawing.Point(12, 95);
+			this.textBoxLexOut.Multiline = true;
+			this.textBoxLexOut.Name = "textBoxLexOut";
+			this.textBoxLexOut.ReadOnly = true;
+			this.textBoxLexOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxLexOut.Size = new System.Drawing.Size(120, 201);
+			this.textBoxLexOut.TabIndex = 2;
+			// 
+			// textBoxMinOut
+			// 
+			this.textBoxMinOut.Location = new System.Drawing.Point(164, 95);
+			this.textBoxMinOut.Multiline = true;
+			this.textBoxMinOut.Name = "textBoxMinOut";
+			this.textBoxMinOut.ReadOnly = true;
+			this.textBoxMinOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBoxMinOut.Size = new System.Drawing.Size(123, 201);
+			this.textBoxMinOut.TabIndex = 2;
+			// 
+			// buttonRun
+			// 
+			this.buttonRun.Location = new System.Drawing.Point(47, 51);
+			this.buttonRun.Name = "buttonRun";
+			this.buttonRun.Size = new System.Drawing.Size(190, 23);
+			this.buttonRun.TabIndex = 3;
+			this.buttonRun.Text = "Сгенерировать перестановки";
+			this.buttonRun.UseVisualStyleBackColor = true;
+			this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(111, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Лексикографически";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(152, 79);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(149, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Минимальным изменением";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(311, 305);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.buttonRun);
+			this.Controls.Add(this.textBoxMinOut);
+			this.Controls.Add(this.textBoxLexOut);
+			this.Controls.Add(this.textBoxNumber);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Генератор перестановок";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxNumber;
+		private System.Windows.Forms.TextBox textBoxLexOut;
+		private System.Windows.Forms.TextBox textBoxMinOut;
+		private System.Windows.Forms.Button buttonRun;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ transpositions/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace transpositions
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonRun_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				lexicographyc();
+				minChange();
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Ошибка, в поле нужно ввести число (лучше,если не больше 9)");
+			}
+		}
+
+
+		private void lexicographyc()
+		{
+			textBoxLexOut.Clear();
+
+			int[] tr = new int[Int32.Parse(textBoxNumber.Text)+1];
+
+			for (int k = 0; k < tr.Length; k++)
+				tr[k] = k;
+
+			int i = 1;
+
+			while(i!=0)
+			{
+				printTransposition(textBoxLexOut, tr, tr.Length-1);
+
+				i = tr.Length-2;
+				while (tr[i] >= tr[i + 1])
+					i--;
+
+				int j = tr.Length-1;
+				while (tr[i] >= tr[j])
+					j--;
+
+				int tmp = tr[i];
+				tr[i] = tr[j];
+				tr[j] = tmp;
+
+				int r = tr.Length - 1;
+				int s = i + 1;
+
+				while(s<= r)
+				{
+					tmp = tr[s];
+					tr[s] = tr[r];
+					tr[r] = tmp;
+					r--;
+					s++;
+				}
+			}
+		}
+
+		private void minChange()
+		{
+			textBoxMinOut.Clear();
+			int n = Int32.Parse(textBoxNumber.Text);
+			int[] tr = new int[n + 2]; // перестановка
+			int[] direction = new int[tr.Length]; // направление движения элементов перестановки
+			int[] ind = new int[tr.Length]; // индексы элементов перестановки
+
+			for (int k = 1; k <= n; k++)
+			{
+				tr[k] = k;
+				direction[k] = -1;
+				ind[k] = k;
+			}
+			tr[0] = tr[n+1] = n+1;
+
+			direction[1] = 0;
+
+			int m = n;
+			while (m != 1)
+			{
+				printTransposition(textBoxMinOut, tr, n);
+
+				m = n;
+				while (tr[ind[m] + direction[m]] > m)
+				{
+					direction[m] = -direction[m];
+					m--;
+				}
+
+				// меняем перестановку
+				int tmp = tr[ind[m]];  //tr[ind[m]] -  m в перестановке
+				tr[ind[m]] = tr[ind[m] + direction[m]]; // tr[ind[m] + direction[m]] - соседний с m слева или справа в зависимости от направления в перестановке				tr[ind[m] + direction[m]] = tmp;
+				tr[ind[m] + direction[m]] = tmp;
+
+				// меняем обратную перестановку
+				tmp = ind[tr[ind[m]]]; // ind[tr[ind[m]]] - индекс того, что встало на место m
+				ind[tr[ind[m]]] = ind[m]; // индекс m
+				ind[m] = tmp;
+			}
+		}
+
+		private void printTransposition(TextBox outBox, int[] tr, int n)
+		{
+			for (int i = 1; i <= n; i++)
+				outBox.AppendText(tr[i].ToString() + " ");
+			outBox.AppendText("\n");
+		}
+	}
+}
